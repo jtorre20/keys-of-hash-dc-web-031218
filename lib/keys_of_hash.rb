0c7b3d1cb8e1b45f133c1f28276dd1687eaf9a8b
jtorre20 <jtorres1989@gmail.com>
2018-02-26 19:42:31 -0500

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,32 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+     array = []
+
+     self.each do |key, value|
+      if arguments.include?(value)
+        array << key
+        #binding.pry
+      end
+     end
+     array
+    end
   end
-end
+    # #binding.pry
+
+    # if arguments.include?("Panama")
+    #   array = ["red-footed tortoise"]
+    #   #binding.pry
+    # elsif arguments.include?("Madagascar")
+    #   array = ["aye-aye", "tomato frog"]
+    # elsif arguments.include?("Australia")
+    #   array = ["sugar glider", "kangaroo", "koala"]
+    # elsif arguments.include?(1)
+    #   arguments.each do |value|
+    #   array << key(value)
+    #   #binding.pry
+    #   end
+    # end
+    # array
+    
+    #binding.pry 
